e025293e7242202bc4fa0e0d83fa3b5abcbf313b
Ashish-Sehray <sehrayashish54@gmail.com>
2025-12-17 15:46:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let angle = 0; 
+let line = document.getElementById('line'); 
+line.style.cssText= `
+    position:absolute;
+	 top:50%;
+	 left:50%;
+    background-color: black;
+    color:black;
+    width:200px;
+	 height:2px;
+`;
+
+setInterval(() => {
+    angle += 2; 
+    line.style.transform = `rotate(${angle}deg)`; 
+}, 20)
